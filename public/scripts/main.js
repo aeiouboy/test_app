8f3875a8967e2250a61a9e2c343dee8e9855ec58
kenji <indytrading@gmail.com>
2016-05-06 17:15:56 +0700

    initial commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,8 @@
-"use strict";console.log("'Allo 'Allo!");
+"use strict";
+
+
+
+  $('.navbar-mobile button span').click(function() {
+      $(this).children('.caret').toggleClass("rotate-180");
+      $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
+  });
